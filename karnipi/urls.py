82d6b7655fa78a111a08324eef28c07058d606ee
bangoperator <mrgrah@googlemail.com>
2016-02-17 09:46:39 +0100

    removed weather url from urls.py

--- karnipi/urls.py
+++ karnipi/urls.py
@@ -1,12 +1,10 @@
+from django.conf.urls import patterns, include, url
 
-from django.conf.urls import patterns, include, url, handler500
 handler500 = 'karnipi.main.views.server_error'
 
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
-from main import views as mainviews
 
 
 admin.autodiscover()
@@ -18,7 +16,6 @@ urlpatterns = patterns(
     url(r'^maintenance/', include('maintenance.urls', namespace="maintenance")),
     url(r'^config/', include('config.urls', namespace="config")),
     url(r'^camera/', include('camera.urls', namespace="camera")),
-    url(r'^weather/', include('weather.urls', namespace="weather")),
     url(r'^terrarium/', include('terrarium.urls', namespace="terrarium")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
