0196eea8568f5474cfdfb33f2c1cc329e34b4802
bangoperator <mrgrah@googlemail.com>
2016-02-17 11:48:33 +0100

    changes

--- main/templatetags/app_filters.py
+++ main/templatetags/app_filters.py
@@ -1,8 +1,7 @@
 from django import template
-from datetime import date, timedelta
 
 register = template.Library()
 
 @register.filter(name='calc_temp')
 def calc_temp(value, arg):
-    return value * 100 / arg
+    return int(value) * 100 / int(arg)
