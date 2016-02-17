9631c9f989a39b8c25aa724e5d32e67826b85468
bangoperator <mrgrah@googlemail.com>
2016-02-17 11:54:40 +0100

    changes

--- start.sh
+++ start.sh
@@ -1,1 +1,9 @@
-/home/bangoperator/karnipi/start.sh
+#!/bin/sh
+
+ROOT=/home/pi/karnipi
+GUNICORN=$ROOT/bin/gunicorn
+GUNICORN_CONF=$ROOT/gunicorn_config.py
+
+#cd $ROOT
+
+#$GUNICORN -c $GUNICORN_CONF karnipi.wsgi
